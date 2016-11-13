c30385c2b4826196163730a67495fc2dc8dee253
Aldo Sanchez <Aldo Sanchez>
2016-11-12 20:29:23 -0600

    created character and enemy arrays + cleaned toggle bool

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,9 +2,11 @@ console.log("star wars!!!")
 
 //define player object:
 var player = {isPlaying:false, wins:0, losses:0};
+var enemyArray = [];
 
 // define a character constructor:
-function character(isPlayer, isOption, isEnemy, health, attackBase,attackPower, counterAttack){
+function character(name, isPlayer, isOption, isEnemy, health, attackBase,attackPower, counterAttack){
+  this.name = name;
   this.isPlayer = isPlayer;
   this.isOption = isOption;
   this.isEnemy = isEnemy;
@@ -15,98 +17,74 @@ function character(isPlayer, isOption, isEnemy, health, attackBase,attackPower, 
 };
 
 // define all characters based on character constructor:
-var darthVader = new character(false, true, false, 200, 30, 30, 45);
-var yoda = new character(false, true, false, 100, 35, 35, 30);
-var luke = new character(false, true, false, 150, 30, 30, 50);
-var chewbacca = new character(false, true, false, 250, 25, 25, 30);
+var darthVader = new character("Darth Vader", false, true, false, 200, 30, 30, 45);
+var yoda = new character("Yoda", false, true, false, 100, 35, 35, 30);
+var luke = new character("Luke", false, true, false, 150, 30, 30, 50);
+var chewbacca = new character("Chewbacca", false, true, false, 250, 25, 25, 30);
+
+charArray = [darthVader, yoda, luke, chewbacca]
 
 //click events for all characters
-
 for (let i = 1; i <= 4; i++){
   $("#char" + i).click(function(){
     if (!player.isPlaying){
   // alert("you clicked " + i);
-    playerChose(i);
+    characterSelection(i);
     }
   });
-}
+};
 
 // function after initial click
-function playerChose(i){
+function characterSelection(i){
   player.isPlaying = !player.Playing;
   switch(i){
+
     case 1:
-      darthVader.isPlayer = !darthVader.isPlayer;
-      darthVader.isOption = !darthVader.isOption;
+      charArray[0].isPlayer = !charArray[0].isPlayer;
+      charArray[0].isOption = !charArray[0].isOption;
+      
+      charArray[1].isEnemy = !charArray[1].isEnemy;      
+      charArray[2].isEnemy = !charArray[2].isEnemy;
+      charArray[3].isEnemy = !charArray[3].isEnemy;
 
-      yoda.isPlayer = !yoda.isPlayer;
-      yoda.isEnemy = !yoda.isEnemy;
-
-      luke.isPlayer = !luke.isPlayer;
-      luke.isEnemy = !luke.isEnemy;
-
-      chewbacca.isPlayer = !chewbacca.isPlayer;
-      chewbacca.isEnemy = !chewbacca.isEnemy;
-
+      enemyArray = [charArray[1], charArray[2],charArray[3]];
       console.log("darthVader");
       break;
 
     case 2:
-      darthVader.isPlayer = false;
-      darthVader.isEnemy = true;
-      darthVader.isOption = true;
+      charArray[1].isPlayer = !charArray[1].isPlayer;
+      charArray[1].isOption = !charArray[1].isOption;
+      
+      charArray[0].isEnemy = !charArray[0].isEnemy;      
+      charArray[2].isEnemy = !charArray[2].isEnemy;
+      charArray[3].isEnemy = !charArray[3].isEnemy;
 
-      yoda.isPlayer = true;
-      yoda.isEnemy = false;
-      yoda.isOption = false;
-
-      luke.isPlayer = false;
-      luke.isEnemy = true;
-      luke.isOption = true;
-
-      chewbacca.isPlayer = false;
-      chewbacca.isEnemy = true;
-      chewbacca.isOption = true;
+      enemyArray = [charArray[0], charArray[2], charArray[3]];
       console.log("yoda");
       break;
 
     case 3:
-      darthVader.isPlayer = false;
-      darthVader.isEnemy = true;
-      darthVader.isOption = true;
+      charArray[2].isPlayer = !charArray[2].isPlayer;
+      charArray[2].isOption = !charArray[2].isOption;
+      
+      charArray[0].isEnemy = !charArray[0].isEnemy;      
+      charArray[1].isEnemy = !charArray[1].isEnemy;
+      charArray[3].isEnemy = !charArray[3].isEnemy;
 
-      yoda.isPlayer = false;
-      yoda.isEnemy = true;
-      yoda.isOption = true;
-
-      luke.isPlayer = true;
-      luke.isEnemy = false;
-      luke.isOption = false;
-
-      chewbacca.isPlayer = false;
-      chewbacca.isEnemy = true;
-      chewbacca.isOption = true;
+      enemyArray = [charArray[0], charArray[1], charArray[3]];
       console.log("luke");
       break;
 
     case 4:
-      darthVader.isPlayer = false;
-      darthVader.isEnemy = true;
-      darthVader.isOption = true;
+      charArray[3].isPlayer = !charArray[3].isPlayer;
+      charArray[3].isOption = !charArray[3].isOption;
+      
+      charArray[0].isEnemy = !charArray[0].isEnemy;      
+      charArray[1].isEnemy = !charArray[1].isEnemy;
+      charArray[2].isEnemy = !charArray[2].isEnemy;
 
-      yoda.isPlayer = false;
-      yoda.isEnemy = true;
-      yoda.isOption = true;
-
-      luke.isPlayer = false;
-      luke.isEnemy = true;
-      luke.isOption = true;
-
-      chewbacca.isPlayer = true;
-      chewbacca.isEnemy = false;
-      chewbacca.isOption = false;
-    console.log("chewbacca");
-    break;
+      enemyArray = [charArray[0], charArray[1], charArray[2]];
+      console.log("chewbacca");
+      break;
   }
-}
-
+};
